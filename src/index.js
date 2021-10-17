fbca8a822fc37610bb3453c6a20228410aa5f95e
= <=>
2021-10-17 01:21:33 -0500

    asd

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require('request')
-const convocatoria = 'no'
+const convocatoria = 'si'
 
 const app = express().use(bodyParser.json());
 
